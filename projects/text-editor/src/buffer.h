e708a71a9859f3cb4de49c78791e5e24e02e8f2e
Thinesh <thinesh.m1996@gmail.com>
2025-08-28 18:00:00 +1000

    w1:d3 exercises and notes on text-editor design

---
+++ projects/text-editor/src/buffer.h
@@ -0,0 +1,10 @@
+/**
+	define MAX_LINES: how many lines allowed in buffer memory
+	buffer data structure options
+		- vector<string> <- preferred
+		- unordered_map<int, string>
+		- map<int, string>
+		- list<string>
+	
+	insertLine(), deleteLine()
+**/
