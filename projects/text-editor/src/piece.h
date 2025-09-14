c1633ca351bbb4e11ce86e2a888c35820b920c13
Thinesh <thinesh.m1996@gmail.com>
2025-09-14 16:43:37 +1000

    initial class structure of pi piece and piece table

---
+++ projects/text-editor/src/piece.h
@@ -0,0 +1,24 @@
+#pragma once
+
+enum class BuffType
+{
+	orig,
+	add,
+}
+
+class Piece
+{
+	public: 
+		constexpr Piece(const BuffType t, const std::size_t s, const std::size_t l) noexcept 
+		: type_(t), start_(s), size_(l);
+
+		[[nodiscard]] constexpr BuffType bufferType() const noexcept { return type_; };
+		[[nodiscard]] constexpr std::size_t start() const noexcept { return start_; };
+		[[nodiscard]] constexpr std::size_t size() const noexcept { return size_; };
+
+
+	private:
+		BuffType type_;
+		std::size_t start_, size_;
+}
+
